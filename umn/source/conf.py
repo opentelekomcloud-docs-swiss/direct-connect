c27905f2cf0e50c70c98e802274e0ffcae9ef814
vladimirhasko <vladimirhasko@gmail.com>
2023-08-16 19:23:27 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata-swiss/tools/generate_doc_confpy.py

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -23,11 +23,21 @@ extensions = [
 
 otcdocs_auto_name = False
 otcdocs_auto_version = False
-otcdocs_search_environment = 'hc_swiss'
 
 project = 'Direct Connect'
 otcdocs_repo_name = 'opentelekomcloud-docs-swiss/direct-connect'
 # Those variables are required for edit/bug links
+
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = ''
+otcdocs_doc_link = ''
+otcdocs_doc_title = ''
+otcdocs_doc_type = ''
+otcdocs_service_category = ''
+otcdocs_service_title = ''
+otcdocs_service_type = ''
+otcdocs_search_environment = 'hc_swiss'
+otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
